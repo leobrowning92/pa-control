c69a1179b6238c9ed42cef37b6c5236481510ae4
leobrowning92 <leobrowning92@gmail.com>
2016-09-21 14:33:21 +1200

    modified the way that the iterator works. first prototype Now the iterator only iterates the entry fields and then inserts at specific string sequences, This is more flexible than finding predefined sequences from the filename and iterating and then stitching the string back together.

--- working_guipull_postprocess/gui.py
+++ working_guipull_postprocess/gui.py
@@ -3,52 +3,41 @@ Tkinter app for running the pa download, cleandata and PAplot scripts
 from a simple GUI
 """
 
-from Tkinter import Tk, Text, BOTH, W, N, E, S, RAISED, StringVar, Menu, Radiobutton, IntVar
+from Tkinter import Tk, Text, BOTH, W, N, E, S, RAISED, StringVar, Menu, Radiobutton, IntVar, OptionMenu
 from ttk import Frame, Button, Label, Style, Entry
 import tkFileDialog
 
+import time
 
 import modules.download_pa as download_pa
 
 
 
+
 ##HELPER FUNCTIONS##
 
-def iterateChip(string):
-    """
-    Iterates the number that X (can be of arbitrary non-sero length)
-    That is situated in a filename of type
-    [SNT]X_deviceY_notesyblah.blah
-    """
-    # start index of the chip number comes after the 3 letter chip
-    # code at the start of the name
-    j = 3
-    k = string[j:].find("_")
+def iterate(instring,iterstring,iterval,pad):
+    """Iterates the iter sequence in the instring, padding to pad number of digits with zeros on the left"""
+    try:
+        int(iterval)
+    except:
+        print("iterval is not an integer string")
+    return instring[:instring.find(iterstring)]+str(int(iterval)+1).zfill(pad)+instring[instring.find(iterstring)+len(iterstring):]
 
-    # Finds the start stop indeces of the device number
-    string = string.replace(
-        string[:j + k],string[:j] + str(int(string[j:j + k]) + 1).zfill(3))
+def insertstring(instring,placeholder,value,pad=0):
+    # assert type(value)==int
+    return instring[:instring.find(placeholder)]+str(value).zfill(pad)+instring[instring.find(placeholder)+len(placeholder):]
 
-    l = string.find("_device") + 7
-    m = string[l:].find("_")
+def timeStampYMDH():
+    # -> 'YYYY_MM_DD_HHMM' as a time stamp
+    return time.strftime('%Y_%m_%d_%H%M')
 
-    # changes to device 1. Must happen before chip change to keep indices good
-    string = string.replace("_device" + string[l:l + m], "_device1")
-    return string
+def make_fname_final(instring,chipval,deviceval):
+    postchip=insertstring(instring,"[chip]",chipval,pad=3)
+    postdev=insertstring(postchip,"[device]",deviceval,pad=2)
+    posttime=insertstring(postdev,"[time]",timeStampYMDH(),pad=0)
+    return posttime
 
-
-def iterateDevice(string):
-    """
-    Iterates the number that Y (can be of arbitrary non-sero length)
-    That is situated in a filename of type
-    blahblah_chipX_deviceY_notesyblah.blah
-    """
-
-    # Finds the start stop indeces of the device number
-    j = string.find("_device") + 7
-    k = string[j:].find("_")
-
-    return string.replace("_device" + string[j:j + k], "_device" + str(int(string[j:j + k]) + 1))
 
 
 # MAIN PAGE CLASS
@@ -63,100 +52,118 @@ class PAGUI(Frame):
     def initUI(self):
         self.parent.title("Parameter Analyzer Control")
         self.style = Style()
-        self.style.theme_use("default")
+        self.style.theme_use("clam")
         self.pack(fill=BOTH, expand=True)
 
         self.columnconfigure(1, weight=1)
         self.columnconfigure(3, pad=7)
-        self.rowconfigure(3, weight=1)
-        self.rowconfigure(5, pad=7)
+        # self.rowconfigure(3, weight=1)
+        # self.rowconfigure(5, pad=7)
 
-        self.path = StringVar()
+        #set up instance variables
+        self.directory = StringVar()
         self.fname = StringVar()
+        self.fname_final=StringVar()
+        self.device = StringVar()
+        self.chip = StringVar()
 
-        self.lbl1 = Label(self, text="Dummy label at the top of the widget",
-                          justify="center")
-        self.lbl1.grid(column=1, sticky=W, pady=4, padx=5)
 
-        self.lbl2 = Label(self, text='path/to/save/to/file',
+        self.toplabel = Label(self, text="Dummy label at the top of the widget",justify="center")
+        self.bottomlabel = Label(self, text="for help/info see github.com/leobrowning92/pa-control",justify="center",font="arial 11 italic")
+
+
+        self.directory_btn = Button(self, text="Directory", command=self.askdirectory)
+
+        self.filename_btn = Button(self, text="Filename", command=self.askfile)
+
+        # self.exit_btn = Button(self, text="Exit", command=self.quit)
+        # self.exit_btn.grid(row=5, column=0, padx=5)
+
+        self.update_btn = Button(self, text="Update", command=self.runUpdate)
+
+        self.iterdevice_btn = Button(self, text="Iterate [device]", command=self.newDevice)
+
+        self.iterchip_btn = Button(self, text="Iterate [chip]", command=self.newChip)
+
+        self.chip.set("001")
+        self.chipnum_entry = Entry(self, textvariable=self.chip,width=5)
+
+        self.device.set("01")
+        self.devicenum_entry = Entry(self, textvariable=self.device,width=5)
+
+        self.directory_entry = Entry(self, textvariable=self.directory)
+
+        self.fname.set('Chip[chip]_device[device]_somenotes_[time].csv')
+        self.fname_final.set(" => "+make_fname_final(self.fname.get(),
+                            self.chip.get(),self.device.get()))
+
+        self.fname_entry = Entry(self, textvariable=self.fname)
+
+        self.fname_final_label = Label(self, textvariable=self.fname_final,
                           justify="center", font="arial 11 italic")
-        self.lbl2.grid(column=1, row=3, columnspan=2,
-                       sticky=N + E + W, pady=4, padx=5)
 
-        self.lbl3 = Label(self, text="Save Path : ",
-                          justify="right")
-        self.lbl3.grid(column=0, row=3, sticky=N + E, pady=4, padx=5)
-
-        self.entry1 = Entry(self, textvariable=self.path)
-        self.entry1.grid(row=1, column=1, columnspan=2,
-                         padx=5, sticky=E + W)
-
-        self.entry2 = Entry(self, textvariable=self.fname)
-        self.entry2.grid(row=2, column=1, columnspan=2,
-                         padx=5, sticky=E + W)
-
-        self.abtn = Button(self, text="Directory", command=self.askdirectory)
-        self.abtn.grid(row=1, column=0)
-
-        self.cbtn = Button(self, text="Filename", command=self.askfile)
-        self.cbtn.grid(row=2, column=0)
-
-        self.hbtn = Button(self, text="Exit", command=self.quit)
-        self.hbtn.grid(row=5, column=0, padx=5)
-
-        self.obtn = Button(self, text="Update", command=self.runClick)
-        self.obtn.grid(row=3, column=3, sticky=N)
-
-        self.obtn = Button(self, text="New Chip", command=self.newChip)
-        self.obtn.grid(row=1, column=3, sticky=N)
-
-        self.obtn = Button(self, text="New Device", command=self.newDevice)
-        self.obtn.grid(row=2, column=3, sticky=N)
 
         # this button runs pulldata with parameter set by
-        # self.v, which stores the value of self.radbtn
-        self.hbtn = Button(self, text="Pull Data", command=self.pulldata)
-        self.hbtn.grid(row=5, column=3, padx=5)
+        # self.datatype, which stores the value of self.radbtn
+        self.pulldata_btn = Button(self, text="Pull Data", command=self.pulldata)
 
-        self.v = IntVar()
-        self.radbtn = Radiobutton(
-            self, text='Diode (VF, IF)', variable=self.v, value=1)
-        self.radbtn.grid(row=5, column=1, padx=5, sticky=N + E)
 
-        self.radbtn = Radiobutton(
-            self, text='FET (VG, VDS, ID, IG)', variable=self.v, value=2)
-        self.radbtn.grid(row=5, column=2, padx=5, sticky=N + E)
+        #datatype=1 => diode, datatype=2 => FET
+        self.datatype = IntVar()
+        self.datatype1_radiobutton = Radiobutton(
+            self, text='Diode (VF, IF)',
+            variable=self.datatype, value=1)
+        self.datatype2_radiobutton = Radiobutton(
+            self, text='FET (VG, VDS, ID, IG)',
+            variable=self.datatype, value=2)
+
+        #grid alignments of all widgets
+        self.toplabel.grid(column=0,columnspan=3, sticky=W, pady=4, padx=5)
+        self.directory_btn.grid(row=1, column=0)
+        self.filename_btn.grid(row=2, column=0)
+        self.update_btn.grid(row=5, column=0, padx=5)
+        self.iterdevice_btn.grid(row=4, column=0, sticky=N)
+        self.iterchip_btn.grid(row=3, column=0, sticky=N)
+        self.chipnum_entry.grid(row=3, column=1,
+                         padx=5, sticky=W)
+        self.devicenum_entry.grid(row=4, column=1,
+                         padx=5, sticky=W)
+        self.directory_entry.grid(row=1, column=1, columnspan=2,
+                         padx=5, sticky=E + W)
+        self.fname_entry.grid(row=2, column=1, columnspan=2,
+                         padx=5, sticky=E + W)
+        self.fname_final_label.grid(column=3, row=2, columnspan=1,
+                       sticky=N + E + W, pady=4, padx=5)
+        self.pulldata_btn.grid(row=5, column=3, padx=5,sticky=E)
+        self.datatype1_radiobutton.grid(row=5,column=1,padx=5,sticky=N + E + S)
+        self.datatype2_radiobutton.grid(row=5,column=2,padx=5,sticky=N + W + S)
+        self.bottomlabel.grid(row=6,column=0,columnspan=3, sticky=W, pady=4, padx=5)
 
     def newChip(self):
-        if self.entry2.get() == '':
+        if self.fname.get() == '':
             self.askfile()
-
         else:
             try:
-                newname = iterateChip(self.entry2.get())
+                self.chip.set(str((int(self.chip.get())+1)).zfill(2))
+                self.device.set(str(1).zfill(2))
             except Exception as e:
-                self.lbl2.config(text="ERROR filename not compatible")
-                newname =self.entry2.get()
                 print(e)
+            self.runUpdate()
 
-            self.entry2.delete(0, 'end')
-            self.entry2.insert(0, newname)
 
     def newDevice(self):
-        if self.entry2.get() == '':
+        if self.fname.get() == '':
             self.askfile()
         else:
             try:
-                newname = iterateDevice(self.entry2.get())
+                self.device.set(str((int(self.device.get())+1)).zfill(2))
             except Exception as e:
-                self.lbl2.config(text="ERROR filename not compatible")
-                newname =self.entry2.get()
                 print(e)
-            self.entry2.delete(0, 'end')
-            self.entry2.insert(0, newname)
+            self.runUpdate()
 
-    def runClick(self):
-        self.lbl2.config(text=(self.entry1.get() + '/' + self.entry2.get()))
+    def runUpdate(self):
+        self.fname_final.set(" => "+make_fname_final(self.fname.get(),
+                            self.chip.get(),self.device.get()))
 
     def askdirectory(self):
         """Returns a selected directoryname."""
@@ -173,10 +180,10 @@ class PAGUI(Frame):
         self.entry2.insert(0, fullpath[i + 1:])
 
     def pulldata(self):
-        if self.v.get() == 1:
+        if self.datatype.get() == 1:
             download_pa.download_data(path=self.entry1.get(
             ), filename=self.entry2.get(), values=['VF', 'IF'])
-        elif self.v.get() == 2:
+        elif self.datatype.get() == 2:
             download_pa.download_data(path=self.entry1.get(
             ), filename=self.entry2.get(), values=['VG', 'VDS', 'ID', 'IG'])
 
@@ -185,7 +192,7 @@ class PAGUI(Frame):
 
 def main():
     root = Tk()
-    root.geometry("800x200+300+300")
+    root.geometry("1000x200+300+300")
     app = PAGUI(root)
     root.mainloop()
 
